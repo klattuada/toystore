c724f4ce3f37b63bf8dce961e21dfd3689b33223
klattuada <90414276+klattuada@users.noreply.github.com>
2024-04-07 00:00:31 -0400

    Updated order.php

--- order.php
+++ order.php
@@ -11,6 +11,51 @@
 	 		  - Return the order info
 	 */
 
+	/*
+	 * Retrieve ord information from the database based on email/order.
+	 * 
+	 * @param PDO $pdo       An instance of the PDO class.
+	 * @param string $id     The ID of the order to retrieve.
+	 * @return array|null    An associative array containing the order information, or null if no toy is found.
+	 */
+	function get_ord(PDO $pdo, string $id) {
+
+		// SQL query to retrieve toy information based on the toy ID
+		$sql = "SELECT * 
+			FROM orders
+			WHERE ordernum = :id;";	// :id is a placeholder for value provided later 
+		                               // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
+
+
+		// Execute the SQL query using the pdo function and fetch the result
+		$order = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+
+		// Return the toy information (associative array)
+		return $order;
+	}
+
+	/*
+	 * Retrieve cust information from the database based on email/order.
+	 * 
+	 * @param PDO $pdo       An instance of the PDO class.
+	 * @param string $id     The emailof the cust to retrieve.
+	 * @return array|null    An associative array containing the cust information, or null if no toy is found.
+	 */
+	function get_cust(PDO $pdo, string $id) {
+
+		// SQL query to retrieve toy information based on the toy ID
+		$sql = "SELECT * 
+			FROM customer
+			WHERE  email = :id;";	// :id is a placeholder for value provided later 
+		                               // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
+
+
+		// Execute the SQL query using the pdo function and fetch the result
+		$cust = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+
+		// Return the toy information (associative array)
+		return $cust;
+	}
 	
 	// Check if the request method is POST (i.e, form submitted)
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -21,11 +66,9 @@
 		// Retrieve the value of the 'orderNum' field from the POST data
 		$orderNum = $_POST['orderNum'];
 
-
-		/*
-		 * TO-DO: Retrieve info about order from the db using provided PDO connection
-		 */
-		
+		$cust_info = get_cust($pdo, $email);
+		$ord_info = get_ord($pdo, $orderNum);
+	
 	}
 // Closing PHP tag  ?> 
 
@@ -89,19 +132,19 @@
 				  -- TO-DO: Check if variable holding order is not empty. Make sure to replace null with your variable!
 				  -->
 				
-				<?php if (!empty(null)): ?>
+				<?php if (!empty($ord_info) and !empty($cust_info)): ?>
 					<div class="order-details">
 
 						<!-- 
 				  		  -- TO DO: Fill in ALL the placeholders for this order from the db
   						  -->
 						<h1>Order Details</h1>
-						<p><strong>Name: </strong> <?= '' ?></p>
-				        	<p><strong>Username: </strong> <?= '' ?></p>
-				        	<p><strong>Order Number: </strong> <?= '' ?></p>
-				        	<p><strong>Quantity: </strong> <?= '' ?></p>
-				        	<p><strong>Date Ordered: </strong> <?= '' ?></p>
-				        	<p><strong>Delivery Date: </strong> <?= '' ?></p>
+						<p><strong>Name: </strong> <?= $cust_info['cname'] ?></p>
+				        	<p><strong>Username: </strong> <?= $cust_info['username'] ?></p>
+				        	<p><strong>Order Number: </strong> <?= $ord_info['ordernum'] ?></p>
+				        	<p><strong>Quantity: </strong> <?= $ord_info['quantity'] ?></p>
+				        	<p><strong>Date Ordered: </strong> <?=$ord_info['date_ordered'] ?></p>
+				        	<p><strong>Delivery Date: </strong> <?= $ord_info['date_deliv'] ?></p>
 				      
 					</div>
 				<?php endif; ?>
